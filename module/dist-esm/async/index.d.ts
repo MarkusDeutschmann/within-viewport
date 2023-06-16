ac8ae25baf20edbb094a01698a95335aed1d6b04
Markus Deutschmann <Markus.Deutschmann@gfk.com>
2023-06-16 09:20:30 +0200

    removed unneded files from dist-esm

--- module/dist-esm/async/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { MultipleSides, Side, UserOptions } from '../common/types.js';
-/**
- * Determines whether an element is within the viewport
- * @param  {HTMLElement}      elem           DOM Element (required)
- * @param  {String | Object}  userOptions    Optional settings
- * @return {Promise<Boolean>}                Whether the element was completely within the viewport
- */
-export declare function withinViewportAsync(elem: HTMLElement, userOptions?: Side | MultipleSides | Partial<UserOptions>): Promise<boolean>;
-//# sourceMappingURL=index.d.ts.map
